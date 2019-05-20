86eec30a1c7fc4d6b5e8910d8b3b3f58d808fb85
Marvin Irwin <marvin@marvinirwin.com>
2019-05-20 10:27:48 -0700

    minimal example

--- test-react/src/App.js
+++ test-react/src/App.js
@@ -14,11 +14,15 @@ const bufferedStdErr$ = stdErr$.pipe(buffer(stdErrFinished$));
 bufferedStdOut$.subscribe(v => console.log(v.map(String.fromCharCode).join('')));
 bufferedStdErr$.subscribe(v => console.warn(v.map(String.fromCharCode).join('')));
 
+
 Test({
   MEMFS : [{name: 'main.c', data: ''}],
   arguments: [],
   stdout: v => stdOut$.next(v),
   stderr: v => stdErr$.next(v),
+  cb: function(files) {
+    alert(JSON.stringify(files));
+  }
 });
 
 function App() {
--- test-react/src/test.js
+++ test-react/src/test.js
@@ -32,7 +32,6 @@ var __ffmpegjs_utf8ToStr;
 __ffmpegjs_utf8ToStr = UTF8ArrayToString;
 __ffmpegjs_opts = Module;
 var __ffmpegjs_return;
-
 function __ffmpegjs_toU8(data) {
     if (Array.isArray(data) || data instanceof ArrayBuffer) {
         data = new Uint8Array(data);
@@ -45,18 +44,16 @@ function __ffmpegjs_toU8(data) {
     }
     return data;
 }
-
 Object.keys(__ffmpegjs_opts).forEach(function (key) {
-    if (key != "mounts" && key != "MEMFS") {
+    if (key != "mounts" && key != "MEMFS" && key != "cb") {
         Module[key] = __ffmpegjs_opts[key];
     }
 });
-
+// Before we run, enter our work directory and create files passed in like {MEMFS: [{name: 'foo.txt', data: new Uint8Array()}]}
 Module["preRun"] = function () {
     FS.mkdir("/work");
     FS.chdir("/work");
-
-    (__ffmpegjs_opts["MEMFS"] || []).forEach(function (file) {
+(__ffmpegjs_opts["MEMFS"] || []).forEach(function (file) {
         if (file["name"].match(/\//)) {
             throw new Error("Bad file name");
         }
@@ -66,26 +63,16 @@ Module["preRun"] = function () {
         FS.close(fd);
     });
 };
-
+// After we're done, search for files in our immediate directory
 Module["postRun"] = function () {
-    // NOTE(Kagami): Search for files only in working directory, one
-    // level depth. Since FFmpeg shouldn't normally create
-    // subdirectories, it should be enough.
     function listFiles(dir) {
         var contents = FS.lookupPath(dir).node.contents;
         var filenames = Object.keys(contents);
-        // Fix for possible file with "__proto__" name. See
-        // <https://github.com/kripken/emscripten/issues/3663> for
-        // details.
-        if (contents.__proto__ && contents.__proto__.name === "__proto__") {
-            filenames.push("__proto__");
-        }
         return filenames.map(function (filename) {
             return contents[filename];
         });
     }
-
-    var inFiles = Object.create(null);
+var inFiles = Object.create(null);
     (__ffmpegjs_opts["MEMFS"] || []).forEach(function (file) {
         inFiles[file.name] = null;
     });
@@ -96,7 +83,10 @@ Module["postRun"] = function () {
         return {"name": file.name, "data": data};
     });
     __ffmpegjs_return = {"MEMFS": outFiles};
+    // Call the callback if we have one
+    __ffmpegjs_opts["cb"] && __ffmpegjs_opts["cb"](__ffmpegjs_return);
 };
+
 
 
 
